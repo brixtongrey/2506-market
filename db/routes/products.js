0c9ccf8e32db9819957c7f32c5e7c3c612d18c98
Brixtongrey <brixtonaruda@gmail.com>
2025-11-09 07:46:02 -0700

    feat: added routes/products.js file

---
+++ db/routes/products.js
@@ -0,0 +1,38 @@
+import express from "express";
+import db from "#db/client";
+import requireUser from "#middleware/requireUser";
+import getUserFromToken from "#middleware/getUserFromToken";
+
+const router = express.Router();
+
+// attach user from token if exists 
+router.use(getUserFromToken);
+
+// get all products
+router.get("/", async (req, res) => {
+  const { rows } = await db.query("SELECT * FROM products");
+  res.send(rows);
+});
+
+// get orders that include product
+router.get("/:id/orders", requireUser, async (req, res) => {
+  const { id } = req.params;
+  const { rows: product } = await db.query(
+    "SELECT * FROM products WHERE id = $1",
+    [id]
+  );
+  if (!product[0]) return res.status(404).send("Product not found");
+
+  const { rows: orders } = await db.query(
+    `SELECT orders.*
+     FROM orders
+     JOIN orders_products ON orders.id = orders_products.order_id
+     WHERE orders_products.product_id = $1 
+     AND orders.user_id = $2`,
+    [id, req.user.id]
+  );
+
+  res.send(orders);
+});
+
+export default router;
